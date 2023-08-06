ebeaf3ab68b612e315151e68037ab199f2e510d5
Dudeldups <130840918+Dudeldups@users.noreply.github.com>
2023-08-06 23:28:54 +0200

    added background colors

--- src/components/Stat/Stat.tsx
+++ src/components/Stat/Stat.tsx
@@ -25,14 +25,23 @@ export default function Stat({ stat, timeframe }: Props) {
       break;
   }
 
+  const fakePopup = () => {
+    alert("This would probably give you more details");
+  };
+
+  const transformedTitle = stat.title.toLowerCase().replace(/\s+/g, "-");
+
+  const imagePath = `/assets/images/icon-${transformedTitle}.svg`;
+
   const { current, previous } = stat.timeframes[timeframe];
 
   return (
     <div className="stat" key={stat.title}>
+      <img className="stat__img" src={imagePath} alt="" />
       <article className="stat__details">
         <div className="stat__top">
           <h2 className="stat__title">{stat.title}</h2>
-          <button className="stat__menu-button">
+          <button className="stat__menu-button" onClick={fakePopup}>
             <img src="/assets/images/icon-ellipsis.svg" alt="" />
             <span className="sr-only">See details for {stat.title}</span>
           </button>
